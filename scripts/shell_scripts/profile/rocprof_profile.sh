9f7e67f0bb94a84a47617ed89fb71821bbf6996d
theanh-ktmt <trantheanh.ktmt@gmail.com>
2024-11-14 19:15:01 +0900

    Add rocprof & rocprofv2 profiler script

---
+++ scripts/shell_scripts/profile/rocprof_profile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export MIOPEN_DISABLE_CACHE=1
+export HIP_VISIBLE_DEVICES=7
+
+rocprof --sys-trace -o profile_output.csv \
+python3 scripts/inference.py configs/anhtt/inference.py \
+    --num-frames 4s --resolution 720p --num-sampling-steps 3 \
+    --prompt "a beautiful vocanic mountain" \
+    --reference-path "save/references/sample.jpg"
