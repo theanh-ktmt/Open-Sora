eebbe865aa079bce49a4ac6c67ab7b0b2dae8fa5
theanh-ktmt <trantheanh.ktmt@gmail.com>
2024-12-02 16:37:26 +0900

    IsolateCK impl

--- opensora/utils/custom/layers/__init__.py
+++ opensora/utils/custom/layers/__init__.py
@@ -3,8 +3,6 @@ import os
 import torch.nn as nn
 from loguru import logger
 
-from .linear import CustomedCKLinear
-
 ENABLE_CK = os.environ.get("ENABLE_CK", "0") == "1"
 
 
@@ -12,6 +10,8 @@ def replace_with_custom_layers(module: nn.Module) -> nn.Module:
     """Replace all module layers with custom layers if ENABLE_CK is set to True."""
     if not ENABLE_CK:
         return module
+
+    from .linear import CustomedCKLinear
 
     def replace_child(child_module: nn.Module) -> nn.Module:
         """Recursively replace layers in the given module."""
