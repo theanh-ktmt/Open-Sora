ee5627f826166bd8850850759eadcb8fcea0d1a3
theanh-ktmt <trantheanh.ktmt@gmail.com>
2024-12-06 19:12:11 +0900

    Add TORCH_TRACE to profile script

--- scripts/shell_scripts/profile/torch_profile.sh
+++ scripts/shell_scripts/profile/torch_profile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-IS_PROFILING=1 TARGET_SAMPLE=2 PROFILE_OUTDIR=save/profile/pt-rocm-tc/mi300-fp16/720p-4s/torch_profile \
-MIOPEN_DISABLE_CACHE=1 HIP_VISIBLE_DEVICES=6 \
-ENABLE_XFORMERS=0 ENABLE_TORCHCOMPILE=1 \
+export IS_PROFILING=1
+export TARGET_SAMPLE=2
+export PROFILE_OUTDIR=save/profile/pt-rocm-tc/mi300-fp16/720p-4s/torch_profile
+export TORCH_TRACE=$PROFILE_OUTDIR
+export MIOPEN_DISABLE_CACHE=1
+export HIP_VISIBLE_DEVICES=1
+export ENABLE_XFORMERS=0
+export ENABLE_TORCHCOMPILE=1
+
 python3 scripts/inference.py configs/anhtt/inference.py \
     --num-frames 4s --resolution 720p --num-sampling-steps 30 \
     --prompt "a beautiful vocanic mountain" --num-sample 3 \
