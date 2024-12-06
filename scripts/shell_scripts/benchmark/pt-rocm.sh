421684f77f53a6a05b4ebb809278855a821f23a3
theanh-ktmt <trantheanh.ktmt@gmail.com>
2024-12-06 19:13:07 +0900

    Update running scripts

--- scripts/shell_scripts/benchmark/pt-rocm.sh
+++ scripts/shell_scripts/benchmark/pt-rocm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 MIOPEN_DISABLE_CACHE=1 ENABLE_XFORMERS=1 HIP_VISIBLE_DEVICES=6 \
 python3 scripts/benchmark_performance.py \
-    configs/anhtt/benchmark_performance.py
+    configs/anhtt/benchmark_performance.py \
+    --flash-attn "true" --layernorm-kernel "false" --dtype "fp16"
