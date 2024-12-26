003419d3a14cbd9398b1100ebbc82bc4553ca0b2
theanh-ktmt <trantheanh.ktmt@gmail.com>
2024-12-26 13:51:40 +0900

    Temporarily remove hooking at compiling

--- opensora/utils/custom/compile.py
+++ opensora/utils/custom/compile.py
@@ -4,18 +4,8 @@ from typing import Any, Dict, Optional
 import torch
 import torch.nn as nn
 from loguru import logger
-from torch._inductor.select_algorithm import extern_kernels
 
 ENABLE_TORCHCOMPILE: Optional[bool] = None
-
-
-def trace_fn_name(fn):
-    def wrapper(*args, **kwargs):
-        # do some side effect to see that the hook is working
-        # print(f"Patched: {fn.__name__=}")
-        return fn(*args, **kwargs)
-
-    return wrapper
 
 
 def is_torch_compile_enabled() -> bool:
@@ -35,11 +25,20 @@ def compile_module(
 ):
     """Implement torch.compile for an nn.Module."""
     # Hook custom kernels to after 'torch.compile'
-    assert extern_kernels.mm is torch.mm
-    assert extern_kernels.addmm is torch.addmm
+    # from torch._inductor.select_algorithm import extern_kernels
 
-    extern_kernels.addmm = trace_fn_name(extern_kernels.addmm)
-    extern_kernels.mm = trace_fn_name(extern_kernels.mm)
+    # def trace_fn_name(fn):
+    #     def wrapper(*args, **kwargs):
+    #         # do some side effect to see that the hook is working
+    #         print(f"Patched: {fn.__name__=}")
+    #         return fn(*args, **kwargs)
+
+    #     return wrapper
+
+    # assert extern_kernels.mm is torch.mm
+    # assert extern_kernels.addmm is torch.addmm
+    # extern_kernels.addmm = trace_fn_name(extern_kernels.addmm)
+    # extern_kernels.mm = trace_fn_name(extern_kernels.mm)
 
     # prepare configs
     DEFAULT_CONFIGS = {
