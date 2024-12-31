13860964f0b91ae020764bd88f683c45746fe74f
theanh-ktmt <trantheanh.ktmt@gmail.com>
2024-12-31 17:08:26 +0900

    update profile script

--- scripts/shell_scripts/profile/torch_profile.sh
+++ scripts/shell_scripts/profile/torch_profile.sh
@@ -2,17 +2,18 @@
 # profile args
 export IS_PROFILING=1
 export TARGET_SAMPLE=2
-export PROFILE_OUTDIR="save/profile/custom/end2end/torch.compile_wo_ck"
-export TORCH_TRACE="./"
+export PROFILE_OUTDIR="save/profile/pt-rocm-tc/mi300-fp16/720p-4s/torch_profile/replaced-attn"
+export TORCH_TRACE="tmp"
 
 # gpus
 export MIOPEN_DISABLE_CACHE=1
-export HIP_VISIBLE_DEVICES=0
+export HIP_VISIBLE_DEVICES=7
 
 # modules
-export ENABLE_XFORMERS=0
+export ENABLE_XFORMERS=1
 export ENABLE_TORCHCOMPILE=1
-export CUSTOM_BACKEND="ck" # or "hipblaslt"
+# export CUSTOM_BACKEND="ck" # or "hipblaslt"
+export TORCHINDUCTOR_UNIQUE_KERNEL_NAMES=1
 
 python3 scripts/inference.py configs/anhtt/inference.py \
     --num-frames 4s --resolution 720p --num-sampling-steps 30 \
