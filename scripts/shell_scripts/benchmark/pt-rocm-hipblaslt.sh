6b06e82a65789a97cf6f3d0c8202274baf31ff27
theanh-ktmt <trantheanh.ktmt@gmail.com>
2025-01-03 17:37:14 +0900

    update inference scripts

--- scripts/shell_scripts/benchmark/pt-rocm-hipblaslt.sh
+++ scripts/shell_scripts/benchmark/pt-rocm-hipblaslt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-MIOPEN_DISABLE_CACHE=1 HIP_VISIBLE_DEVICES=1 \
-CUSTOM_BACKEND="hipblaslt" ENABLE_XFORMERS=0 ENABLE_TORCHCOMPILE=1 \
+# CUSTOM_GEMM_TUNING_WARMUP=1 CUSTOM_GEMM_TUNING_ITER=1 \
+MIOPEN_DISABLE_CACHE=1 HIP_VISIBLE_DEVICES=7 \
+CUSTOM_BACKEND="hipblaslt" ENABLE_XFORMERS=1 ENABLE_TORCHCOMPILE=1 \
 python3 scripts/benchmark_performance.py \
     configs/anhtt/benchmark_performance.py \
     --flash-attn "true" --layernorm-kernel "false" --dtype "fp16"
